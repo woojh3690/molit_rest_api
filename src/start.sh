0f220b08f833b472685c6760860a091d99724ea7
woojh3690 <woojh3690@gmail.com>
2022-07-21 18:15:27 +0900

    start.sh 스크립트 수정

--- src/start.sh
+++ src/start.sh
@@ -1,2 +1,2 @@
-nohup gunicorn --bind 0.0.0.0:8000 --workers 4 --thread 9 --timeout=600 rest_api_main:app > server.log&
-nohup gunicorn --keyfile ../conf/emqx.key --certfile ../conf/emqx.pem --bind 0.0.0.0:8001 --workers 4 --thread 9 --timeout=600 rest_api_main:app > server_tls.log&
+nohup gunicorn --workers 4 --thread 9 --timeout=600 --bind 0.0.0.0:8000 rest_api_main:app > server.log&
+nohup gunicorn --keyfile ../conf/emqx.key --certfile ../conf/emqx.pem --workers 4 --thread 9 --timeout=600 --bind 0.0.0.0:8001 rest_api_main:app > server_tls.log&

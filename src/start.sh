9c280f23fac6a0dd4fa202c0272a4ebea32dfba9
woojh3690 <woojh3690@gmail.com>
2022-07-21 17:51:53 +0900

    http, https 동시 지원

--- src/start.sh
+++ src/start.sh
@@ -1,1 +1,2 @@
-nohup gunicorn --keyfile ../conf/emqx.key --certfile ../conf/emqx.pem --bind 0.0.0.0:8000 --workers 4 --thread 9 --timeout=600 rest_api_main:app > server.log &
+nohup gunicorn --bind 0.0.0.0:8000 --workers 4 --thread 9 --timeout=600 rest_api_main:app > server.log&
+nohup gunicorn --keyfile ../conf/emqx.key --certfile ../conf/emqx.pem --bind 0.0.0.0:8001 --workers 4 --thread 9 --timeout=600 rest_api_main:app > server_tls.log&

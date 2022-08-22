7bce570181c92a0171087584b8a16314d3533855
woojh3690 <woojh3690@gmail.com>
2022-08-22 11:45:35 +0900

    http 만 사용하게 변경

--- src/start.sh
+++ src/start.sh
@@ -1,2 +1,1 @@
 nohup gunicorn --workers 4 --thread 9 --timeout=600 --bind 0.0.0.0:8000 rest_api_main:app > server.log&
-nohup gunicorn --keyfile ../conf/emqx.key --certfile ../conf/emqx.pem --workers 4 --thread 9 --timeout=600 --bind 0.0.0.0:8001 rest_api_main:app > server_tls.log&
